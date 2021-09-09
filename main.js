d095cec7d0c5acea04e7fb473435fdf67773e8c5
BrisbanePokerUnited <90361001+BrisbanePokerUnited@users.noreply.github.com>
2021-09-09 10:34:37 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+var modal = document.getElementById("myModal2");
+var modal = document.getElementById("myModal3");
+var modal = document.getElementById("myModal4");
+var modal = document.getElementById("myModal5");
+var modal = document.getElementById("myModal6");
+var modal = document.getElementById("myModal7");
+var modal = document.getElementById("myModal8");
+var modal = document.getElementById("myModal9");
+var modal = document.getElementById("myModal10");
+var modal = document.getElementById("myModal11");
+var modal = document.getElementById("myModal12");
+var modal = document.getElementById("myModal13");
+var modal = document.getElementById("myModal14");
+var modal = document.getElementById("myModal15");
+var modal = document.getElementById("myModal16");
+var modal = document.getElementById("myModal17");
+var modal = document.getElementById("myModal18");
+var modal = document.getElementById("myModal19");
+var modal = document.getElementById("myModal20");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+var btn = document.getElementById("myBtn2");
+var btn = document.getElementById("myBtn3");
+var btn = document.getElementById("myBtn4");
+var btn = document.getElementById("myBtn5");
+var btn = document.getElementById("myBtn6");
+var btn = document.getElementById("myBtn7");
+var btn = document.getElementById("myBtn8");
+var btn = document.getElementById("myBtn9");
+var btn = document.getElementById("myBtn10");
+var btn = document.getElementById("myBtn11");
+var btn = document.getElementById("myBtn12");
+var btn = document.getElementById("myBtn13");
+var btn = document.getElementById("myBtn14");
+var btn = document.getElementById("myBtn15");
+var btn = document.getElementById("myBtn16");
+var btn = document.getElementById("myBtn17");
+var btn = document.getElementById("myBtn18");
+var btn = document.getElementById("myBtn19");
+var btn = document.getElementById("myBtn20");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+
+
+<script>
+      $('.navbar-collapse a').click(function(){
+          $(".navbar-collapse").collapse('hide')};
+      );
+</script>
+
+}
